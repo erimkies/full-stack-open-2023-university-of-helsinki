a296f207e5c4c6eb0beee014d33a85d25e8a8d21
Erricos <erimkies@gmail.com>
2023-08-07 01:50:10 +0300

    Resubmitting Exercise 1.4 to better reflect the requirements of the task

--- Part 1/courseinfo/Exercise 1.4/src/App.js
+++ Part 1/courseinfo/Exercise 1.4/src/App.js
@@ -20,9 +20,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <Part name={props.name} exercises={props.exercises}/>
-      <Part name={props.name1} exercises={props.exercises1}/>
-      <Part name={props.name2} exercises={props.exercises2}/>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   )
 }
@@ -31,7 +31,7 @@ const Total = (props) => {
   console.log(props)
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   )
 }
@@ -58,8 +58,8 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content name={parts[0].name} exercises={parts[0].exercises} name1={parts[1].name} exercises1={parts[1].exercises} name2={parts[2].name} exercises2={parts[2].exercises}/>
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
